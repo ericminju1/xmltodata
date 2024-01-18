c1c0a24ed22a635ce04f8af5d0cf7fa196cc2bc2
MJ <mj@wonderstic.kr>
2024-01-18 05:52:29 +0000

    remove midiToAudio: always follow the process of MIDI->Synth->Audio

--- musicxml_parser/generate_dataset.py
+++ musicxml_parser/generate_dataset.py
@@ -1,14 +1,5 @@
-from midiToAudio import midiToAudio
 from midiToSynth import midiToSynth
 from synthToAudio import synthToAudio
-
-def generate_dataset(path_format, save_format, splits, insts=4):
-    for split_num in range(splits):
-        for inst_num in range(insts):
-            midiToAudio(path_format.format(split_num, inst_num, "roll"),
-                        path_format.format(split_num, inst_num, "art"),
-                        save_path=save_format.format(split_num, inst_num))
-            
 
 def generate_synth(path_format, save_format, splits, insts=4):
     for split_num in range(splits):
